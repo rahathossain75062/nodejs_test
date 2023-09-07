6435ab701ae302e249d898d42a185569df32ff13
sujitbaram <sujitbaram@gmail.com>
2023-09-07 19:36:52 +0530

    gpttest

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// src/App.js
+import React, { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <h1>React Testing Example</h1>
+      <p>Count: {count}</p>
+      <button onClick={incrementCount}>Increment</button>
+    </div>
+  );
+}
+
+export default App;
