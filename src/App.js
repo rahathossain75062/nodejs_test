09a3ce3c09070dccfc239e96a72643badd335ea2
rahathossain75062 <rahathossain75062@gmail.com>
2023-09-11 14:50:54 -0500

    First

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <div>
-      <h1>React Testing Example CICD BY Rahat "HI NOW"</h1>
+      <h1>React Testing Example CICD BY Rahat "HI NOW Now"</h1>
       <p>Count: {count}</p>
       <button onClick={incrementCount}>Increment</button>
     </div>
